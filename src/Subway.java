856bb12c17114a11ba0d3499b44fad6bc4a54aca
yeonjae <yeonjae978@gmail.com>
2023-05-04 18:19:25 +0900

    세번째 커밋

--- src/Subway.java
+++ src/Subway.java
@@ -4,7 +4,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Subway {
@@ -25,7 +29,7 @@ public class Subway {
     while ((str = br.readLine()) != null) {
       inputListStr.add(br.readLine());
     }
-    System.out.println(inputListStr);
+    // System.out.println(inputListStr);
     // String lines = br.readLine();
 
     // System.out.println(lines);
@@ -34,9 +38,30 @@ public class Subway {
     // inputList<String.stream().map(String >> Map) : List<Map<String, Object>>
     // 경계
     // 집계함수 (max, min, sum, avg, count)
+    for (int i = 0; i < inputListStr.size(); i++) {
+      Object[] obj = inputListStr.get(i).split(",");
+      System.out.println(Arrays.toString(obj));
+      // System.out.println(obj.to);
+      // obj[0]
+
+    }
+    // inputListStr.stream().map(s -> {
+    // Map<String, Object> map = new HashMap<>();
+    // map.put(s, str)
+    // return null;
+    // })
+    // Map<String, Object> map = new HashMap<>();
+    // // Object[] obj = inputListStr.
+    // inputListStr.stream().forEach(System.out::println);
+    // inputListStr.stream().map(s -> map.put(s. , s))
 
     // 날짜별(10일단위)로 출력
-    inputListStr.stream().collect(
-        Collectors.groupingBy(null, null, null));
+    // inputListStr.stream().collect(
+    // Collectors.groupingBy(
+    // // 그룹 대상
+    // inputListStr.,
+    // // 형태(타입)
+    // null,
+    // null));
   }
 }
